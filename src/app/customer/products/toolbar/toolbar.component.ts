0c2cba7df26ba719d4a67dfbaf60b21f8997084b
Sam <49291807+Sam-Eldin@users.noreply.github.com>
2022-08-27 14:24:30 +0300

    + toolbar improve

--- src/app/customer/products/toolbar/toolbar.component.ts
+++ src/app/customer/products/toolbar/toolbar.component.ts
@@ -16,6 +16,7 @@ export class ToolbarComponent implements OnInit {
   @Output() optionsEventEmitter: EventEmitter<IOptions> = new EventEmitter();
 
   options: IOptions;
+  arrowDirection: string = 'arrow_upward';
 
   constructor() {
     this.options = {
@@ -37,9 +38,10 @@ export class ToolbarComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  setSortBy(event: any) {
-    let optionText = event.target.value;
-    this.options.sortAsc = optionText === 'l2h';
+  setSortBy(_: any) {
+    const downward = this.arrowDirection === 'arrow_downward';
+    this.options.sortAsc = downward;
+    this.arrowDirection = downward ? 'arrow_upward' : 'arrow_downward';
     this.optionsEventEmitter.emit(this.options);
   }
 
